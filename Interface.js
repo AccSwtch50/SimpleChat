3eb323ab8517c6cb6ab1a86e1f85b94d3a00f882
AccSwtch50 <101040181+AccSwtch50@users.noreply.github.com>
2026-07-03 14:44:45 +0700

    Added MCP dropdown

--- Interface.js
+++ Interface.js
@@ -14,6 +14,10 @@ function model_dropdown_clicked() {
 	document.getElementById("model-dropdown-list").classList.toggle("show-dropdown");
 }
 
+function mcp_tools_clicked() {
+	document.getElementById("mcp-list").classList.toggle("show-dropdown");
+}
+
 function close_dropdowns_if_unfocused() {
 	const dropdowns = document.getElementsByClassName("dropdown-content");
 	for (const dropdown of dropdowns) {
